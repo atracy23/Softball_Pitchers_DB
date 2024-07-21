8378be62be61b43b8663025edb089698a58e886a
atracy23 <111539642+atracy23@users.noreply.github.com>
2024-07-21 12:13:25 -0500

    Changes to the Footer and Header.  Making responsive.

--- Footer.php
+++ Footer.php
@@ -14,7 +14,7 @@ This program creates a footer for all pages.
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml" style="height: 100%;">
 <head>
-  <link href="CSS.css" rel="stylesheet" type="text/css" />
+  <!--link href="LandingPageStyle.css" rel="stylesheet" type="text/css" /-->
   <title>Server-Side Scripting</title>
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -22,9 +22,9 @@ This program creates a footer for all pages.
   </head>
 <body style="height: 100%;">
 
-<footer style="background-color: silver; margin-top: 20px;">
-    <a href="http://localhost/CSD_440/Database-baseball_01/AngieIndex.php" style="float: right; font-weight: 400; padding: 10px; color:darkblue; text-decoration: underline;"><i>Back to Home</i></a>
-    <a href="http://localhost/CSD_440/Database-baseball_01" style="float: right; font-weight: 400; padding: 10px; color:darkblue; text-decoration: underline;"><i>Back to Baseball Index</i></a>
+<footer>
+    <a href="http://localhost/Softball_Pitchers_DB/AngieIndex.php" style="float: right; font-weight: 400; padding: 10px; color:darkblue; text-decoration: underline;"><i>Back to Home</i></a>
+    <a href="http://localhost/Softball_Pitchers_DB" style="float: right; font-weight: 400; padding: 10px; color:darkblue; text-decoration: underline;"><i>Back to Softball Index</i></a>
     <p style="padding: 10px;">&#169; 2023 Tracy</p>
 </footer>
 </body
--- Header.php
+++ Header.php
@@ -9,43 +9,36 @@ Module 10 Programming Assignment
 This program creates a nav bar for all pages.
 -->
 
-
 <!DOCTYPE html>
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml" style="height: 100%;">
+
 <head>
-  <link href="CSS.css" rel="stylesheet" type="text/css" />
+  <link href="NavBarStyle.css" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Server-Side Scripting</title>
-  <nav class="overlay">
+</head>
+
+<body>
+  <nav>
     <div class="navbar-container">
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+          <i class="fas fa-bars"></i>
+      </label>
       <div class="logo">
-        <img src="http://localhost/CSD_440/Database-baseball_01/logo.jpg" alt="Logo">
+        <img src="http://localhost/Softball_Pitchers_DB/logo.jpg" alt="Logo">
         <h2 class="title">Softball Pitchers and Stats</h2>
       </div>
       <ul class="navbar">
-        <li class="active"><a href="index.jsp">Home</a></li>
-        <li>
-          <a href="#">Table</a>
-          <ul>
-            <li><a href="AngieCreateTable.php">Create Table</a></li>
-            <li><a href="AngieDropTable.php">Drop Table</a></li>
-            <li><a href="AngiePopulateTable.php">Populate Table</a></li>
-          </ul>
-        <li>
+        <li><a class="active" href="AngieIndex.php">Home</a></li>
         <li><a href="AngieQueryTable.php">Stats Table</a> </li>
-        <li><a href="AngieQuery.php">Search Stats</a>
-        <li><a href="AngieForm.php">Add Stats</a>
+        <li><a href="AngieQuery.html">Search Stats</a>
+        <li><a href="AngieForm.html">Add Stats</a>
       </ul>
     </div>
   </nav>
- 
-
-
-
-  </head>
-
-  
-
+</body>
 
 </html>
