9673ecf061663b09eabc42597c9f3b099d6aab37
atracy23 <111539642+atracy23@users.noreply.github.com>
2024-07-21 12:16:17 -0500

    Changes to the home page.  Making more responsive and removing populate table function.  Added to create table function.

--- AngieIndex.php
+++ AngieIndex.php
@@ -14,47 +14,32 @@ This program uses PHP to create an index file with links to all of the php files
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml" style="height: 100%;">
 <head>
   <link rel="stylesheet" type="text/css" href="LandingPageStyle.css" />
+  <link rel="stylesheet" type="text/css" href="TableStyle.css" />
+  <link rel="stylesheet" type="text/css" href="ImageButtonStyle.css" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 
   <title>Server-Side Scripting</title>
-
 </head>
 
-<body style="height: 100%;">
-<!-- Creating navigation bar -->
-  <nav class="overlay">
-    <div class="navbar-container">
-      <div class="logo">
-        <img src="http://localhost/CSD_440/Database-baseball_01/logo.jpg" alt="Logo">
-        <h2 class="title">Softball Pitchers and Stats</h2>
-      </div>
-      <ul class="navbar">
-        <li class="active"><a href="AngieIndex.php">Home</a></li>
-        <li><a href="AngieQueryTable.php">Table</a> </li>
-        <li><a href="AngieQuery.html">Search</a></li>
-        <li><a href="AngieForm.html">Add/Edit Stats</a></li>
-      </ul>
-    </div>
-  </nav>
+<body style="height: 100%; background-image: url('http://localhost/Softball_Pitchers_DB/blue_background.jpg')">
 
+<?php include 'Header.php' ?>
+  
     <div class="image-text-container">
         <h3 class="text-line">Options</h3><br>
         <ul>
           <li><a href="AngieCreateTable.php">Create Table</a></li>
           <li><a href="AngieDropTable.php">Drop Table</a></li>
-          <li><a href="AngiePopulateTable.php">Populate Table</a></li>
           <li><a href="AngieQueryTable.php">See Stats Table</a></li>
           <li><a href="AngieQuery.html">Search for Pitcher Stats</a></li>
           <li><a href="AngieForm.html">Add New Pitcher or Stats to Table</a></li>
           <li><a href="AngiePDF.php" target="_blank">Create a PDF File of Table</a></li>
         </ul>
       </div>
-    </div>
   
 <?php include 'Footer.php' ?>
-
 
 </body>
 </html>
